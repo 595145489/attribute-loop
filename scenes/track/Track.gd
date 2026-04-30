943fd903b5b98e81e8fb2d780e0bed05a26d070c
sihan.wang <sihan.wang@happyelements.com>
2026-04-30 19:31:28 +0800

    feat: Track generates Array[Tile] tiles at _ready via tile_count

--- scenes/track/Track.gd
+++ scenes/track/Track.gd
@@ -1,13 +1,19 @@
 class_name Track
 extends Node2D
 
+const Tile = preload("res://scripts/core/Tile.gd")
+
 @export var loop_points: Array[Vector2] = []
+@export var tile_count: int = 12
 @onready var visual: Line2D = $TrackVisual
+
+var tiles: Array = []
 
 func _ready() -> void:
 	if loop_points.is_empty():
 		_build_default_track()
 	_draw_track()
+	_generate_tiles()
 
 func _build_default_track() -> void:
 	var cx = 640.0
@@ -31,6 +37,13 @@ func _draw_track() -> void:
 	pts.append(pts[0])
 	visual.points = PackedVector2Array(pts)
 
+func _generate_tiles() -> void:
+	tiles.clear()
+	for i in tile_count:
+		var tile = Tile.new()
+		tile.track_t = (float(i) + 0.5) / float(tile_count)
+		tiles.append(tile)
+
 func get_position_at(t: float) -> Vector2:
 	var total = loop_points.size()
 	var scaled = t * total
@@ -45,3 +58,6 @@ func get_total_length() -> float:
 		var next = (i + 1) % loop_points.size()
 		length += loop_points[i].distance_to(loop_points[next])
 	return length
+
+func get_tile_index_for_t(t: float) -> int:
+	return int(t * tile_count) % tile_count
