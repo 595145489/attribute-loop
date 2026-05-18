aa6a00f9f71dbe07b843dc8c73b97aea2107c77d
sihan.wang <sihan.wang@happyelements.com>
2026-05-18 19:44:05 +0800

    chore: add TestHelper autoload for screenshot-based integration testing

---
+++ src/autoloads/TestHelper.gd
@@ -0,0 +1,24 @@
+extends Node
+
+const SENTINEL := "res://tests/.test_mode"
+const SCREENSHOT_PATH := "res://tests/screenshots/last_run.png"
+const LOG_PATH := "res://tests/screenshots/last_run.log"
+const WAIT_SECONDS := 3.0
+
+func _ready() -> void:
+	if not FileAccess.file_exists(SENTINEL):
+		return
+	await get_tree().create_timer(WAIT_SECONDS).timeout
+	_capture_and_quit()
+
+func _capture_and_quit() -> void:
+	var image := get_viewport().get_texture().get_image()
+	image.save_png(SCREENSHOT_PATH)
+
+	var log_file := FileAccess.open(LOG_PATH, FileAccess.WRITE)
+	if log_file:
+		log_file.store_line("screenshot saved: %s" % SCREENSHOT_PATH)
+		log_file.store_line("time: %s" % Time.get_datetime_string_from_system())
+		log_file.close()
+
+	get_tree().quit()
