1110eb29e217d87547aba61ef5f58cd29307f2ea
sihan.wang <sihan.wang@happyelements.com>
2026-05-21 19:57:53 +0800

    feat: add visited_this_loop to Tile; emit tile_passed in Main._check_player_tile

--- src/Main.gd
+++ src/Main.gd
@@ -12,37 +12,42 @@ const GAME_OVER_SCENE = preload("res://scenes/ui/game_over.tscn")
 @onready var game_loop: GameLoop = $GameLoop
 
 func _ready() -> void:
-	var tiles = _build_tiles()
-	player.setup(player_follow, track)
-	game_loop.setup(tiles, enemies_container, player, combat_system)
-	EventBus.player_died.connect(_on_player_died)
+    var tiles = _build_tiles()
+    player.setup(player_follow, track)
+    game_loop.setup(tiles, enemies_container, player, combat_system)
+    EventBus.player_died.connect(_on_player_died)
 
 func _build_tiles() -> Array:
-	var tiles: Array = []
-	var curve = track.curve
-	var length = curve.get_baked_length()
-	for i in 12:
-		var t = float(i) / 12.0
-		var pos = curve.sample_baked(t * length)
-		var tile: Tile = TILE_SCENE.instantiate()
-		tile.position = pos
-		tile.tile_index = i
-		tiles_container.add_child(tile)
-		tiles.append(tile)
-	return tiles
+    var tiles: Array = []
+    var curve = track.curve
+    var length = curve.get_baked_length()
+    for i in 12:
+        var t = float(i) / 12.0
+        var pos = curve.sample_baked(t * length)
+        var tile: Tile = TILE_SCENE.instantiate()
+        tile.position = pos
+        tile.tile_index = i
+        tiles_container.add_child(tile)
+        tiles.append(tile)
+    return tiles
 
 func _process(_delta: float) -> void:
-	if GameState.is_paused:
-		return
-	_check_player_tile()
+    if GameState.is_paused:
+        return
+    _check_player_tile()
 
 func _check_player_tile() -> void:
-	var player_pos = player.global_position
-	for tile in tiles_container.get_children():
-		if tile.has_enemy() and player_pos.distance_to(tile.global_position) < 30.0:
-			game_loop.check_tile_for_enemy(tile)
-			return
+    var player_pos = player.global_position
+    for tile in tiles_container.get_children():
+        if player_pos.distance_to(tile.global_position) < 30.0:
+            if tile.has_enemy():
+                game_loop.check_tile_for_enemy(tile)
+                return
+            if not tile.visited_this_loop:
+                tile.visited_this_loop = true
+                EventBus.tile_passed.emit(tile.tile_index)
+            return
 
 func _on_player_died() -> void:
-	var go = GAME_OVER_SCENE.instantiate()
-	add_child(go)
+    var go = GAME_OVER_SCENE.instantiate()
+    add_child(go)
--- src/entities/Tile.gd
+++ src/entities/Tile.gd
@@ -2,13 +2,14 @@ class_name Tile
 extends Node2D
 
 var tile_index: int = 0
-var enemy: Enemy = null   # null when tile is unoccupied
+var enemy: Enemy = null
+var visited_this_loop: bool = false
 
 func has_enemy() -> bool:
-	return enemy != null
+    return enemy != null
 
 func place_enemy(e: Enemy) -> void:
-	enemy = e
+    enemy = e
 
 func clear_enemy() -> void:
-	enemy = null
+    enemy = null
