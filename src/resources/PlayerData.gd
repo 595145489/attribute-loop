cff470437ae080d4eba24feb0c2742d8d94807b8
sihan.wang <sihan.wang@happyelements.com>
2026-06-16 10:26:27 +0800

    feat: set player dmg_base to 15

--- src/resources/PlayerData.gd
+++ src/resources/PlayerData.gd
@@ -2,6 +2,6 @@ class_name PlayerData
 extends Resource
 
 @export var hp_base: int = 10000
-@export var dmg_base: int = 100
+@export var dmg_base: int = 15
 @export var attack_interval: float = 0.8   # seconds between player attacks
 @export var walk_speed: float = 120.0      # pixels per second along path
